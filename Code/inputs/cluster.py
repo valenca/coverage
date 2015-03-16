74c23a348abe261acb9c3b3d81262deabcf48264
jsvalenca <jsvalenca@gmail.com>
2015-03-16 15:21:09 +0000

    added files

---
+++ Code/inputs/cluster.py
@@ -0,0 +1,24 @@
+from sys import argv
+from random import random,seed
+from math import sqrt
+from scipy.spatial import distance
+
+N=int(argv[2])
+K=15
+S=10
+seed((argv[1],argv[2]))
+
+#print N
+#print K
+k=0
+for i in range(K):
+	x=(random()*100,random()*100)
+	j=1
+	print k,x[0],x[1]
+	k+=1
+	while(j < (N/K)+1):
+		y=(random()*S*2)+x[0]-S,(random()*S*2)+x[1]-S
+		if(distance.euclidean(x,y)<=S):
+			print k,y[0],y[1]
+			j+=1
+			k+=1
